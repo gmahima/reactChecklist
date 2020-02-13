eaf5cc781e443e4786d8246c2e2b9aeb3dd1a699
gmahima <mahimagangavarapu@gmail.com>
2020-02-13 22:46:47 +0530

    fix bug

--- src/context/index.js
+++ src/context/index.js
@@ -2,9 +2,9 @@ import React, {createContext, useState} from 'react';
 export const ChecklistContext = createContext(null);
 export function Provider(props) {
     const [checklist, setChecklist] = useState([
-        {title: 'naruto', id: '0naruto', checked:false},
-        {title: 'snk', id: '01snk', checked:false},
-        {title: 'gekkan shoujo nozaki kun', id: '02nozaki', checked:false}
+        {title: 'naruto', id: '0naruto'},
+        {title: 'snk', id: '01snk'},
+        {title: 'gekkan shoujo nozaki kun', id: '02nozaki'}
     ])
     const handleCheck = (id, val) => {
         console.log("1) "+ id + " : " + val)
